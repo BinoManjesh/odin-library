4791c8e7d555ae2dc404377fb5c2a1f6d7ca943f
BinoManjesh <binomanjesh@gmail.com>
2023-09-07 13:40:13 +0530

    Add display of books

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 "use strict"
 
-console.log("Hello World!")
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.getDOMNode = function() {
+    const book = document.createElement("div");
+    book.className = "book";
+    const list = document.createElement("ul");
+    book.appendChild(list);
+
+    function getListItem(text) {
+        const listItem = document.createElement("li");
+        listItem.innerText = text;
+        return listItem;
+    }
+
+    [`Title: ${this.title}`, `Author: ${this.author}`, `Pages: ${this.pages}`,
+        `Read: ${this.read ? "Yes" : "No"}`]
+        .forEach((x)=>list.appendChild(getListItem(x)));
+    return book;
+};
+
+let library = [new Book("Bruh", "bino", 69, true), new Book("HooHah", "jeff", "420", false)];
+const container = document.querySelector("div.container");
+library.forEach((x)=>container.appendChild(x.getDOMNode()));
