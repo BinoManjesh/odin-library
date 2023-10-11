9346f323eb044e1fd89da360a23725edf20c7899
BinoManjesh <binomanjesh@gmail.com>
2023-10-11 12:03:05 +0530

    Use classes instead of object factories

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 "use strict"
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    getDOMNode(index) {
+        const book = document.createElement("div");
+        book.dataset.index = index;
+        book.className = "book";
+        const list = document.createElement("ul");
+        list.className = "book";
+        book.appendChild(list);
+
+        function getListItem(text) {
+            const listItem = document.createElement("li");
+            listItem.innerText = text;
+            return listItem;
+        }
+
+        [`"${this.title}"`, `by ${this.author}`, `${this.pages} Pages`,
+            `${this.read ? "Read" : "Not Read"}`]
+            .forEach((x)=>list.appendChild(getListItem(x)));
+        
+        const button = document.createElement("button")
+        button.textContent = "×"
+        const li = document.createElement("li");
+        li.appendChild(button);
+        list.appendChild(li);
+        function onRemove(e) {
+            if (e.target != button) {
+                return;
+            }
+            library.splice(this.dataset.index, 1);
+            this.remove();
+            for (const [index, node] of Array.from(container.children).entries()) {
+                node.dataset.index = index;
+            }
+        }
+        book.addEventListener("click", onRemove);
+        return book;
+    }
 }
-
-Book.prototype.getDOMNode = function(index) {
-    const book = document.createElement("div");
-    book.dataset.index = index;
-    book.className = "book";
-    const list = document.createElement("ul");
-    list.className = "book";
-    book.appendChild(list);
-
-    function getListItem(text) {
-        const listItem = document.createElement("li");
-        listItem.innerText = text;
-        return listItem;
-    }
-
-    [`"${this.title}"`, `by ${this.author}`, `${this.pages} Pages`,
-        `${this.read ? "Read" : "Not Read"}`]
-        .forEach((x)=>list.appendChild(getListItem(x)));
-    
-    const button = document.createElement("button")
-    button.textContent = "×"
-    const li = document.createElement("li");
-    li.appendChild(button);
-    list.appendChild(li);
-    function onRemove(e) {
-        if (e.target != button) {
-            return;
-        }
-        library.splice(this.dataset.index, 1);
-        this.remove();
-        for (const [index, node] of Array.from(container.children).entries()) {
-            node.dataset.index = index;
-        }
-    }
-    book.addEventListener("click", onRemove);
-    return book;
-};
 
 let library = [new Book("Bruh", "bino", 69, true), new Book("HooHah", "jeff", "420", false)];
 const container = document.querySelector("div.container");
